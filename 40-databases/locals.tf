0d3a179cc33124849da4e315dcdb828bf4ec02ca
pramod <peruripramoddevops@gmail.com>
2025-11-03 14:06:20 +0530

    Terraform Dev Infra

---
+++ 40-databases/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  ami_id = data.aws_ami.joindevops.id
+  common_name_suffix = "${var.project_name}-${var.environment}" # roboshop-dev
+  mongodb_sg_id = data.aws_ssm_parameter.database_subnet_ids.value
+  database_subnet_id = split("," , data.aws_ssm_parameter.database_subnet_ids.value)[0]
+    common_tags = {
+        Project = var.project_name
+        Environment = var.environment
+        Terraform = "true"
+}
+}
