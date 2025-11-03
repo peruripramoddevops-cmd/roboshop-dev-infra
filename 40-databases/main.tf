cdc319a4be6c9e62856e8dc341ce81e7529119f7
pramod <peruripramoddevops@gmail.com>
2025-11-03 14:19:07 +0530

    Terraform Dev Infra

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mongodb" {
     ami = local.ami_id
     instance_type = "t3.micro"
-    vpc_security_group_ids = [local.database.subnet_id]
+    vpc_security_group_ids = [local.database_subnet_id]
     subnet_id = local.database_subnet_id
     
     tags = merge (
