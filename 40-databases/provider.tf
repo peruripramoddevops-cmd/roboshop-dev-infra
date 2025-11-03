9ea3b59e7f6aaab3b579e64845ab2745680de069
pramod <peruripramoddevops@gmail.com>
2025-11-03 19:27:39 +0530

    terraform

--- 40-databases/provider.tf
+++ 40-databases/provider.tf
@@ -8,9 +8,8 @@ terraform {
 
   backend "s3" {
     bucket = "pramod-daws"
-    key    = "roboshop-dev-databases"
+    key    = "roboshop-dev-backend-alb"
     region = "us-east-1"
-    use_lockfile = true
     encrypt = true
   }
 }
