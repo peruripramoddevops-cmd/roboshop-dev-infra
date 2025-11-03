af61ba30163bdb6ea24770d1338b962b83565305
pramod <peruripramoddevops@gmail.com>
2025-11-03 14:59:44 +0530

    Terraform Dev Infra

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -20,6 +20,10 @@ data "aws_ami" "joindevops" {
     }
 }
 
+data "aws_ssm_parameter" "database_sg_id" {
+  name = "/${var.project_name}/${var.environment}/database_sg_id"
+}
+
 data "aws_ssm_parameter" "mongodb_sg_id" {
   name = "/${var.project_name}/${var.environment}/mongodb_sg_id"
 }
