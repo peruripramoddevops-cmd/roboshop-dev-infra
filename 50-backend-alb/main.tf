a527fb6c1fbcd776624029a5f3d4a28e6373905d
pramod <peruripramoddevops@gmail.com>
2025-11-03 11:44:03 +0530

    Terraform Dev Infra

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "backend_alb" {
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   # it should be private subnet ids
-  subnets            = local.public_subnet_ids
+  subnets            = local.private_subnet_ids
 
   enable_deletion_protection = false # prevents accidental deletion from UI
 
