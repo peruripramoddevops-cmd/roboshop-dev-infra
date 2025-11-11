82e7215ae4e4efdbbc7d0d2699a03d5196ecceb5
pramod <peruripramoddevops@gmail.com>
2025-11-11 15:30:50 +0530

    terraform-infra

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "pramod.shop"
+}
+
+variable "zone_id" {
+    default = "Z05714843SO1YQWMF1N5N"
+}
