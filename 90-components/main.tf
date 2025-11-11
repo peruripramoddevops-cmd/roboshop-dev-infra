373f287a2095ebb931ef9ccbfb1c80ab985a61c5
pramod <peruripramoddevops@gmail.com>
2025-11-11 17:17:54 +0530

    terraform-infra

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -6,7 +6,9 @@
  */
 module "components" {
     for_each = var.components
-    source = "git::https://github.com/daws-86s/terraform-roboshop-component.git?ref=main"
+    source = "git::https://github.com/peruripramoddevops-cmd/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
+
+
